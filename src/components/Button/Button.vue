0dfaa2b3850068c24803e37f1d0d9d57d0b3c90a
YnDarksy <1920224395@qq.com>
2025-04-04 15:38:07 +0800

    first commit

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <button class="yd-button" :class="{
+    [`yd-button--${type}`]: type,
+    [`yd-button--${size}`]: size,
+    'is-plain': plain,
+    'is-round': round,
+    'is-circle': circle,
+    'is-disabled': disabled
+  }" :disabled="disabled">
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: "YButton",
+})
+</script>
+<script setup lang="ts">
+import type { ButtonProps } from './types.ts'
+defineProps<ButtonProps>()
+</script>
+<style scoped></style>
